db208272415ffdfc57dba6a32475fbfa3dcb3fba
Volodymyr Karpenko <48981117+vladimir98vk@users.noreply.github.com>
2022-09-15 15:01:04 +0200

    export of BasicShape and Square models

--- lib/cat_interpreter.dart
+++ lib/cat_interpreter.dart
@@ -5,8 +5,10 @@
 library cat_interpreter;
 
 export "src/interpreter.dart" show CATInterpreter;
+export "src/models/basic_shape.dart" show BasicShape;
 export "src/models/cross.dart" show Cross;
 export "src/models/schemes.dart";
+export "src/models/square.dart" show Square;
 export "src/results.dart" show Results;
 export "src/utils/errors.dart" show CatError;
 export "src/utils/helper.dart";
